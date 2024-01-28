e744a82a2366150e8d60bd5f81ce1eb4ddde1b04
RaeGyu <wogns9769@gmail.com>
2024-01-28 16:47:16 +0900

    PlayerLookAt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,76 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
-
-    int num = 1;
     int speed = 0;
     public Text ModeText;
     Rigidbody rigid;
+    Vector3 difference;
+    Camera ViewCamera;
     // Start is called before the first frame update
     void Start()
     {
         speed = 3;
         rigid = GetComponent<Rigidbody>();
+        difference = Camera.main.transform.position - transform.position;
+        ViewCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.LeftControl)){
-            num++;
-            switch(num%3){
-                case 1:ModeText.text="Translate";break;
-                case 2: ModeText.text ="AddForce"; break;
-                case 0: ModeText.text = "Velocity"; break;
-            }
-        }
-        if(num%3==1){
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(-speed * Time.deltaTime, 0, 0);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(speed * Time.deltaTime, 0, 0);
-            }
-            else if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Translate(0, 0, speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(0, 0, -speed * Time.deltaTime);
-            }
-        }
-        if(num%3==2){
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rigid.AddForce(-speed, 0, 0);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rigid.AddForce(speed, 0, 0);
-            }
-            else if (Input.GetKey(KeyCode.UpArrow))
-            {
-                rigid.AddForce(0, 0, speed);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rigid.AddForce(0, 0, -speed);
-            }
-        }
-        if(num%3==0){
-            float inputX = Input.GetAxis("Horizontal");
-            float inputZ = Input.GetAxis("Vertical");
-            // -1 ~ 1
+        move();
+        //CameraPostion();
+        LookAt();
+        
+    }
 
-            Vector3 velocity = new Vector3(inputX, 0, inputZ);
-            velocity *= speed;
-            rigid.velocity = velocity;
+    void move(){
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputZ = Input.GetAxisRaw("Vertical");
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity *= speed;
+        rigid.velocity = velocity;
+    }
+    void CameraPostion(){
+        Camera.main.transform.position = transform.position + difference;
+    }
+    void LookAt(){
+        Ray ray = ViewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up,Vector3.zero);
+        float rayDistance;
+
+        if(ground.Raycast(ray,out rayDistance)){
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin,point,Color.red);
+            Vector3 Anti_Tilting_Position = new Vector3(point.x,transform.position.y,point.z);
+            transform.LookAt(Anti_Tilting_Position);
         }
+        
     }
 }
